fb07a60d6ea1ebcf8da8c3c511b4233e228c7cb5
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-09 20:13:10 +0200

    example updated

--- example/src/main.cxx
+++ example/src/main.cxx
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <libhtml/html_node.hxx>
+#include <print>
 
 auto main(int, char**) -> int {
-    std::cout << "Hello from example!" << std::endl;
+    static constexpr auto raw {
+        R"(<div id="container" data-active><span>Content</span></div>)"};
+
+    std::println("Parsing node:");
+    std::println("{}\n", raw);
+
+    const auto node {HtmlNode::decode(raw)};
+
+    std::println("Encoded:");
+    std::println("{}\n", node.encode());
+
     return 0;
 }
