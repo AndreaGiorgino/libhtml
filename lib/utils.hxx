8764fde3bbb07677563ae7f1bd8d5da0db5859dc
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-09 20:12:55 +0200

    stream utilities implemented

--- lib/utils.hxx
+++ lib/utils.hxx
@@ -2,6 +2,66 @@
 #ifndef H_UTILS
 #define H_UTILS
 
-namespace utils {}
+#include <functional>
+#include <istream>
+
+using size = std::size_t;
+
+namespace utils {
+/**
+ * @brief Trim a string
+ *
+ * @param str The input string
+ */
+[[nodiscard]] auto trim(std::string_view str) noexcept -> std::string;
+
+/**
+ * @brief Skip whitespaces in stream
+ *
+ * @param is The input stream
+ */
+auto skipws(std::istream& is) noexcept -> void;
+
+/**
+ * @brief Skip whitespaces and comments in stream
+ *
+ * @param is The input stream
+ *
+ * @throws libhtml::ParseError When an unclosed comment is found
+ */
+auto skipwsAndComments(std::istream& is) -> void;
+
+/**
+ * @brief Throws when the stream has reached EOF
+ *
+ * @param is The input stream
+ *
+ * @throws libhtml::ParseError When the stream has reached EOF
+ */
+auto throwWhenEof(std::istream& is) -> void;
+
+/**
+ * @brief Throws when the next character is not the expected one
+ *
+ * @param is The input stream
+ * @param ch The expected character
+ *
+ * @throws libhtml::ParseError When the next character is not the expected one
+ */
+auto throwWhenNot(std::istream& is, char ch) -> void;
+
+[[nodiscard]] auto getUntil(std::istream& is,
+                            std::function<bool(char)> predicate) noexcept
+    -> std::string;
+
+/**
+ * @brief Add indentation to stream
+ *
+ * @param os The output stream
+ * @param indent The indentation to add
+ */
+auto indent(std::ostream& os, size indent) noexcept -> void;
+
+} // namespace utils
 
 #endif
--- src/utils.cxx
+++ src/utils.cxx
@@ -1,3 +1,98 @@
+#include <cstring>
 #include <lib/utils.hxx>
+#include <libhtml/parse_error.hxx>
 
-namespace utils {}
+namespace utils {
+auto trim(std::string_view str) noexcept -> std::string {
+    if (str.empty())
+        return {};
+
+    return {
+        std::find_if(str.begin(), str.end(),
+                     [](char ch) { return !isspace(ch); }),
+        std::find_if(str.rbegin(), str.rend(),
+                     [](char ch) { return !isspace(ch); })
+            .base(),
+    };
+}
+
+auto skipws(std::istream& is) noexcept -> void {
+    while (isspace(is.peek()))
+        is.ignore();
+}
+
+auto skipwsAndComments(std::istream& is) -> void {
+    while (!is.eof()) {
+        if (isspace(is.peek())) {
+            // handle whitespace
+            is.ignore();
+        } else if (is.peek() == '<') {
+            // handle comment
+            static constexpr auto prefix {"<!--"};
+
+            const auto start {is.tellg()};
+
+            // check whether prefix matches
+            for (size i {}; i < strlen(prefix) && !is.eof(); i++)
+                if (is.get() != prefix[i]) {
+                    is.seekg(start);
+                    break;
+                }
+
+            if (is.tellg() == start)
+                break;
+
+            auto end {start};
+
+            while (!is.eof())
+                if (is.get() == '-' && is.get() == '-') {
+                    throwWhenNot(is, '>');
+
+                    end = is.tellg();
+                    is.ignore();
+                    break;
+                }
+
+            if (end == start)
+                throw ParseError("unclosed comment opened at position {}",
+                                 (int)start);
+        } else
+            break;
+    }
+}
+
+auto throwWhenEof(std::istream& is) -> void {
+    if (!is.eof())
+        return;
+
+    throw ParseError("unexpected end of file at position {}", (int)is.tellg());
+}
+
+auto throwWhenNot(std::istream& is, char ch) -> void {
+    throwWhenEof(is);
+
+    if (is.peek() == ch)
+        return;
+
+    throw ParseError("expected character '{}' at position '{}' (got '{}')", ch,
+                     (int)is.tellg(), (char)is.peek());
+}
+
+auto getUntil(std::istream& is, std::function<bool(char)> predicate) noexcept
+    -> std::string {
+    std::string buffer {};
+
+    while (!is.eof()) {
+        if (predicate(is.peek()))
+            break;
+
+        buffer += is.get();
+    }
+
+    return buffer;
+}
+
+auto indent(std::ostream& os, size indent) noexcept -> void {
+    os << std::string(indent, ' ');
+}
+} // namespace utils
