b627fb50ade0a30d30e464596758d4ed9852b689
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-10 14:31:56 +0200

    test HtmlNode encode

---
+++ CTest/Test_HtmlNode_Encode.cxx
@@ -0,0 +1,42 @@
+#include <libhtml/html_node.hxx>
+
+auto Test_HtmlNode_Encode(int, char**) -> int {
+    HtmlNode node {"span"};
+
+    node.prop("prop-1", "value-1").prop("prop-2", "value-2");
+    node.addChild("content ").addChild(HtmlNode {"b"}.addChild("bold"));
+
+    {
+        // indent 0
+        const auto expected {
+            R"(<span prop-2="value-2" prop-1="value-1">content <b>bold</b></span>)"};
+
+        if (const auto encoded {node.encode(0)}; encoded != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected encoded node:\n"
+                            "{}\n"
+                            "got:\n"
+                            "{}",
+                            __FILE__, __LINE__, expected, encoded));
+    }
+
+    {
+        // indent 4
+        const auto expected {R"(<span prop-2="value-2" prop-1="value-1">
+    content 
+    <b>
+        bold
+    </b>
+</span>)"};
+
+        if (const auto encoded {node.encode()}; encoded != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected encoded node:\n"
+                            "{}\n"
+                            "got:\n"
+                            "{}",
+                            __FILE__, __LINE__, expected, encoded));
+    }
+
+    return 0;
+}
