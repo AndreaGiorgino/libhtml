1c7bf845681de47c9b5ccf05be08b3778b59726e
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-10 14:57:00 +0200

    test HtmlDoc ctor implemented

---
+++ CTest/Test_HtmlDoc_Ctor.cxx
@@ -0,0 +1,43 @@
+#include <libhtml/html_doc.hxx>
+
+auto Test_HtmlDoc_Ctor(int, char**) -> int {
+    const HtmlDoc doc {};
+
+    {
+        // test head
+        const auto head {doc.head()};
+
+        if (const auto expected {"head"}; head.tagName() != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected tag name '{}' (got '{}')",
+                            __FILE__, __LINE__, expected, head.tagName()));
+        else if (const auto expected {0}; head.propsSize() != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected {} properties (got '{}')",
+                            __FILE__, __LINE__, expected, head.propsSize()));
+        else if (const auto expected {0}; head.childrenSize() != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected {} children (got '{}')", __FILE__,
+                            __LINE__, expected, head.childrenSize()));
+    }
+
+    {
+        // test body
+        const auto body {doc.body()};
+
+        if (const auto expected {"body"}; body.tagName() != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected tag name '{}' (got '{}')",
+                            __FILE__, __LINE__, expected, body.tagName()));
+        else if (const auto expected {0}; body.propsSize() != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected {} properties (got '{}')",
+                            __FILE__, __LINE__, expected, body.propsSize()));
+        else if (const auto expected {0}; body.childrenSize() != expected)
+            throw std::runtime_error(
+                std::format("{}:{}: expected {} children (got '{}')", __FILE__,
+                            __LINE__, expected, body.childrenSize()));
+    }
+
+    return 0;
+}
