54a103be746fff6cf9b8872c02353a5f8d5cb7d8
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-10 14:56:35 +0200

    HtmlDoc get/set methods implemented

--- include/libhtml/html_doc.hxx
+++ include/libhtml/html_doc.hxx
@@ -3,16 +3,36 @@
 #define H_HTML_DOC
 
 #include <istream>
+#include <libhtml/html_node.hxx>
 
+/**
+ * @class HtmlDoc
+ * @brief Represents an html document
+ *
+ */
 class HtmlDoc final {
    public:
     // ------------------------------------------------------------------------
     // Ctors
     // ------------------------------------------------------------------------
-    HtmlDoc(void) noexcept = default;
+    HtmlDoc(HtmlNode head = {"head"}, HtmlNode body = {"body"});
 
+    /**
+     * @brief Decode an html document from stream
+     *
+     * @param is The input stream
+     *
+     * @throws libhtml::ParseError When any parsing error has occured
+     */
     [[nodiscard]] static auto decode(std::istream& is) -> HtmlDoc;
 
+    /**
+     * @brief Decode an html document from string
+     *
+     * @param raw The input string
+     *
+     * @throws libhtml::ParseError When any parsing error has occured
+     */
     [[nodiscard]] static auto decode(std::string_view raw) -> HtmlDoc;
 
     // ------------------------------------------------------------------------
@@ -31,6 +51,39 @@ class HtmlDoc final {
     // Dtor
     // ------------------------------------------------------------------------
     ~HtmlDoc(void) noexcept = default;
+
+   public:
+    // ------------------------------------------------------------------------
+    // Lookup
+    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Get the document head
+     */
+    [[nodiscard]] auto head(void) const noexcept -> HtmlNode;
+
+    /**
+     * @brief Get the document body
+     */
+    [[nodiscard]] auto body(void) const noexcept -> HtmlNode;
+
+    // ------------------------------------------------------------------------
+    // Modifiers
+    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Get the document head
+     */
+    auto head(HtmlNode head) -> void;
+
+    /**
+     * @brief Get the document body
+     */
+    auto body(HtmlNode head) -> void;
+
+   private:
+    HtmlNode _head {};
+    HtmlNode _body {};
 };
 
 #endif
--- src/html_doc.cxx
+++ src/html_doc.cxx
@@ -1,5 +1,21 @@
+#include <cstring>
+#include <lib/utils.hxx>
+#include <libhtml/doc_error.hxx>
 #include <libhtml/html_doc.hxx>
+#include <libhtml/parse_error.hxx>
 #include <sstream>
+
+HtmlDoc::HtmlDoc(HtmlNode head, HtmlNode body) : _head(head), _body(body) {
+    if (head.tagName() != "head")
+        throw DocError(
+            "'html' first child must be an 'head' tag (got '{}' instead)",
+            head.tagName());
+
+    if (body.tagName() != "body")
+        throw DocError(
+            "'html' second child must be a 'body' tag (got '{}' instead)",
+            body.tagName());
+}
 
 auto HtmlDoc::decode(std::istream& is) -> HtmlDoc {
     return {}; // FIXME: parse data
@@ -10,4 +26,30 @@ auto HtmlDoc::decode(std::string_view raw) -> HtmlDoc {
     ss << raw;
 
     return HtmlDoc::decode(ss);
+}
+
+auto HtmlDoc::head(void) const noexcept -> HtmlNode {
+    return _head;
+}
+
+auto HtmlDoc::body(void) const noexcept -> HtmlNode {
+    return _body;
+}
+
+auto HtmlDoc::head(HtmlNode head) -> void {
+    if (head.tagName() != "head")
+        throw DocError(
+            "'html' first child must be an 'head' tag (got '{}' instead)",
+            head.tagName());
+
+    _head = head;
+}
+
+auto HtmlDoc::body(HtmlNode body) -> void {
+    if (body.tagName() != "body")
+        throw DocError(
+            "'html' second child must be a 'body' tag (got '{}' instead)",
+            body.tagName());
+
+    _body = body;
 }
